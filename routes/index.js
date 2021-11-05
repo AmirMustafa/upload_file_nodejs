adaa880a8d5b9ac7a113fd2ac0f8d94a20ef865e
Amir Mustafa <amirmustafa@Amirs-MacBook-Air.local>
2021-11-05 16:34:14 +0530

    feat: single file upload

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const upload = require("../common");
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.post("/single_upload", upload.single("image"), (req, res, next) => {
+  console.log(req.file);
+  res.send("uploaded successfully");
 });
 
 module.exports = router;
